a91d982d6faba77961e4e6d0a1559dd1d083cb51
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-04-25 15:00:26 +0200

    product, summary and demand basic serializers

--- backend/products_and_meals/api/serializers.py
+++ backend/products_and_meals/api/serializers.py
@@ -1,8 +1,18 @@
 from rest_framework import serializers
-from products_and_meals.models import Product
+from products_and_meals.models import (Product, Summary, Demand)
 
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = ['product_id', 'name', 'calories_per_hundred_grams', 'protein', 'fat', 'carbohydrates']
+
+class SummarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Summary
+        fields = ['user_id', 'daily_calory_intake', 'date', 'protein', 'fat', 'carbohydrates']
+
+class DemandSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Demand
+        fields = ['user_id', 'daily_calory_demand', 'date', 'protein', 'fat', 'carbohydrates']
