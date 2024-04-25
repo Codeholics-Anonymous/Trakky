5353eb3eaff976bc8937b6e94509e276164d4ea7
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-04-25 15:01:11 +0200

    urls to http methods of product, summary and demand

--- backend/products_and_meals/api/urls.py
+++ backend/products_and_meals/api/urls.py
@@ -3,14 +3,23 @@ from products_and_meals.api.views import (
                 api_detail_product_view,
                 api_update_product_view,
                 api_delete_product_view,
-                api_create_product_view
+                api_create_product_view,
+                api_detail_summary_view,
+                api_detail_demand_view,
+                api_create_demand_view
             )
 
 app_name = "products_and_meals"
 
 urlpatterns = [
-    path('<int:product_id>/', api_detail_product_view, name='detail'),
-    path('<int:product_id>/update', api_update_product_view, name='update'),
-    path('<int:product_id>/delete', api_delete_product_view, name='delete'),
-    path('create', api_create_product_view, name='create'),
+    # PRODUCT URLS
+    path('product/<int:product_id>/', api_detail_product_view, name='product_detail'),
+    path('product/<int:product_id>/update', api_update_product_view, name='product_update'),
+    path('product/<int:product_id>/delete', api_delete_product_view, name='product_delete'),
+    path('product/create', api_create_product_view, name='product_create'),
+    # SUMMARY URLS
+    path('summary/<int:user_id>/<str:start_date>/<str:end_date>/', api_detail_summary_view, name='summary_detail'),
+    # DEMAND URLS
+    path('demand/<int:user_id>/<str:date>/', api_detail_demand_view, name='demand_detail'),
+    path('demand/create/', api_create_demand_view, name='demand_create')
 ]
