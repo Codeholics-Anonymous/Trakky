e641e4c719002fd6f9a93fb07e8f499e089adceb
j3drzejo <jirla30@gmail.com>
2024-04-25 16:00:41 +0200

    front template

---
+++ frontend/App.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <>
+      <Text>Hello World</Text>
+    </>
+  );
+}
