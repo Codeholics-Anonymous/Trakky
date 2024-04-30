0b17aa25bc92c42daf47a8950beaef48465ce06f
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-04-30 19:08:18 +0200

    register Meal table at admin panel

--- backend/products_and_meals/admin.py
+++ backend/products_and_meals/admin.py
@@ -6,6 +6,7 @@ from .models import Macros, Product,Demand,Summary
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('product_id', 'name', 'calories_per_hundred_grams', 'protein', 'fat', 'carbohydrates')
+
 @admin.register(Demand)
 class DemandAdmin(admin.ModelAdmin):
     list_display = ('user_id', 'date', 'protein', 'carbohydrates', 'fat','daily_calory_demand')
@@ -13,3 +14,7 @@ class DemandAdmin(admin.ModelAdmin):
 @admin.register(Summary)
 class SummaryAdmin(admin.ModelAdmin):
     list_display = ('user_id', 'date', 'daily_calory_intake')
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    list_display = ('meal_id', 'user_id', 'type', 'date')
