ebae856a2390c3174e6eae0de94f9ffd3e947399
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-05-07 22:26:39 +0200

    register userprofile in admin mode

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from user.models import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfile(admin.ModelAdmin):
+    list_display = ['userprofile_id', 'user_id', 'sex', 'weight', 'height', 'activity_level', 'daily_calory_demand']
