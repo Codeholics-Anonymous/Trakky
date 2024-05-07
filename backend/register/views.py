e1a1a9e297a3d9949c3a965007873a8f9ff3775b
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-05-07 19:47:22 +0200

    login, register and test_token

--- backend/register/views.py
+++ backend/register/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response 
+
+from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+@api_view(['POST'])
+def login(request):
+    try:
+        user = User.objects.get(username=request.data['username'])
+    except User.DoesNotExist:
+        return Response({"user" : "not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    if not user.check_password(request.data['password']):
+        return Response({"user" : "not found"}, status=status.HTTP_404_NOT_FOUND)
+    
+    token, created = Token.objects.get_or_create(user=user) # get or create token if it wasn't create yet. It returns tuple (token, True/False)
+    serializer = UserSerializer(user)
+    return Response({"token" : token.key, "user" : serializer.data})
+
+@api_view(['POST'])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=request.data['username'])
+        user.set_password(request.data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+        return Response({"token" : token.key, "user" : serializer.data})
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def test_token(request):
+    return Response(data={"passed for {}".format(request.user.email)})
+
