df15ee60a611f2e5f2d09c8342d47ef818e5bb0d
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-05-07 22:27:21 +0200

    UserProfile with update_profile method

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    userprofile_id = models.AutoField(primary_key=True)
+    user_id = models.IntegerField(null=False, blank=False)
+    sex = models.CharField(max_length=1)
+    weight = models.FloatField(null=True, blank=True, default=0.0)
+    height = models.IntegerField(null=True, blank=True, default=0)
+    activity_level = models.FloatField(null=True, blank=True, default=1)
+    daily_calory_demand = models.IntegerField(null=True, blank=True, default=0.0)
+
+    @classmethod
+    def update_profile(cls, serializer):
+        if serializer.is_valid():
+            serializer.save()
+            return True
+        else:
+            return False
+
+    def calculate_demand(self):
+        ...
