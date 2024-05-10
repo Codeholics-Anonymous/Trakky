3466598ff4d3feddb962fabbe6bd0df6b388465b
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-05-10 08:45:12 +0200

    UserProfile files

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
-# Create your models here.
-
 class UserProfile(models.Model):
     userprofile_id = models.AutoField(primary_key=True)
     user_id = models.IntegerField(null=False, blank=False)
-    sex = models.CharField(max_length=1)
+    sex = models.CharField(max_length=1, default='-')
     weight = models.FloatField(null=True, blank=True, default=0.0)
     height = models.IntegerField(null=True, blank=True, default=0)
     activity_level = models.FloatField(null=True, blank=True, default=1)
+    birth_date = models.DateField(null=True, blank=True, default='2000-01-01')
     daily_calory_demand = models.IntegerField(null=True, blank=True, default=0.0)
+    user_goal = models.CharField(max_length=10, null=True, blank=True, default=0)
 
     @classmethod
     def update_profile(cls, serializer):
@@ -19,5 +19,8 @@ class UserProfile(models.Model):
         else:
             return False
 
-    def calculate_demand(self):
-        ...
+    @classmethod
+    def calculate_demand(cls, weight, height, birth_date, activity_level):
+        today = date.today()
+        age = today.year - birth_date.year - ((today.month, today.day) < (birth_date.month, birth_date.day))
+        return activity_level*((10 * weight) + (6.25 * height) + (5 * age))
