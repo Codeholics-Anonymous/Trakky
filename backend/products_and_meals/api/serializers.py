5218d809070a321ec96b014f7f364b102304f1bd
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-05-12 11:41:56 +0200

    exclude user_id from Product serializer

--- backend/products_and_meals/api/serializers.py
+++ backend/products_and_meals/api/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 from products_and_meals.models import (Product, Summary, Demand, Meal, MealItem)
+from django.contrib.auth.models import User
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        exclude = ['product_id']
+        exclude = ['product_id', 'user_id']
 
 class SummarySerializer(serializers.ModelSerializer):
     class Meta:
