79285ba0438618b091d752cfb3049862ee6a3ce2
j3drzejo <jirla30@gmail.com>
2024-05-14 20:54:15 +0200

    added navigation in app

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,28 @@
-import { View, Text, Image } from "react-native";
-import { Login } from "./screens/Login";
-import { Register } from "./screens/Register";
-export default function App() {
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Login } from './screens/Login';
+import { Register } from './screens/Register';
+
+function HomeScreen() {
   return (
-    <View>
-      <Register></Register>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Register" component={Register} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
