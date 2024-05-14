12d4e621db999038da437188a4233c664b9c2882
j3drzejo <jirla30@gmail.com>
2024-05-14 20:57:58 +0200

    forget to remove compontent from testign

--- frontend/App.js
+++ frontend/App.js
@@ -4,14 +4,6 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Login } from './screens/Login';
 import { Register } from './screens/Register';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
 const Stack = createNativeStackNavigator();
 
 function App() {
