bac7b53a1aeacd0fa5e0507b7721e956902779ca
iamk-cper <kacper.mycka02@gmail.com>
2024-05-14 21:00:09 +0200

    Revert "file convertion into nativewind and some new features"
    This reverts commit a3c169a292cef5f1d6573fb7b50baae48f557e1e.

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -1,20 +1,28 @@
-import React from 'react';
-import { SafeAreaView, StatusBar } from 'react-native';
-import DateDisplay from '../components/DateDisplay.js';
-import Header from '../components/Header.js';
-import MealBox from '../components/MealBox.js';
-import ProgressCircle from '../components/ProgressCircle.js';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView } from 'react-native';
+import DateDisplay from './components/DateDisplay.js';
+import Header from './components/Header.js';
+import MealBox from './components/MealBox.js';
+import ProgressCircles from './components/ProgressCircles.js';
+import styles from './styles/styles.js';
+
 
 export function HomeScreen() {
+  const [statusBarColor, setStatusBarColor] = useState('#000000');
+
+  useEffect(() => {
+    setStatusBarColor('#111111'); // Evening (Dark Slate Gray)
+  }, []);
+
   return (
-    <SafeAreaView className="bg-dark-gray h-full items-center pt-safe-top" style={{paddingTop: StatusBar.currentHeight}}>
-      <Header />
-      <ProgressCircle />
-      <DateDisplay />
-      <MealBox title="Breakfast" />
-      <MealBox title="Brunch" />
-      <MealBox title="Lunch" />
-      <MealBox title="Dinner" />  
-    </SafeAreaView>
+    <SafeAreaView style={styles.container}>
+        <Header />
+        <ProgressCircles />
+        <DateDisplay />
+        <MealBox title="Breakfast" />
+        <MealBox title="Brunch" />
+        <MealBox title="Lunch" />
+        <MealBox title="Dinner" />  
+      </SafeAreaView>
   );
 };
