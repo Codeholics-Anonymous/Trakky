3d87ac85a5727c8b450776042990c32065c20ca3
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-05-14 16:14:49 +0200

    utils directory created

---
+++ backend/utils/user_utils.py
@@ -0,0 +1,18 @@
+from rest_framework.response import Response
+from rest_framework import status
+
+# PASSWORD VALIDATION METHOD
+
+def password_validation(password):
+    # check if password is alphanumeric
+    if (not password.isalnum() or (len(password) < 8) or password.isdigit()):
+        return 0
+    # check if password contains at least one digit
+    contain_digit = False
+    for x in password:
+        if x.isdigit():
+            contain_digit=True
+            break
+    if not contain_digit:
+        return 0
+    return 1
