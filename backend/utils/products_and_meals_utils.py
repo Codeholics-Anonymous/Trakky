6bb77a5fc549af366c93ec47502faa23a3539324
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-05-14 20:18:45 +0200

    above_upper_limit function to check if sum of args isn't larger than limit

--- backend/utils/products_and_meals_utils.py
+++ backend/utils/products_and_meals_utils.py
@@ -2,3 +2,13 @@ from products_and_meals.models import Demand
 
 def find_basic_demand(user_id):
     return Demand.objects.filter(user_id=user_id).order_by('demand_id').first()
+
+# check if sum of args isn't larger than limit
+def above_upper_limit(*args, limit):
+    sum = 0
+    for x in args:
+        sum += x
+    if sum > limit:
+        return True
+    else:
+        return False
