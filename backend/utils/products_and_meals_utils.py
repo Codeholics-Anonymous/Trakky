469eb4a2248b8180c02c4c641f65be760de31cff
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-05-15 14:08:41 +0200

    find_mealitems util added

--- backend/utils/products_and_meals_utils.py
+++ backend/utils/products_and_meals_utils.py
@@ -1,4 +1,5 @@
-from products_and_meals.models import Demand
+from products_and_meals.models import Demand, Meal, MealItem
+from django.db.models import Q
 
 def find_basic_demand(user_id):
     return Demand.objects.filter(user_id=user_id).order_by('demand_id').first()
@@ -12,3 +13,16 @@ def above_upper_limit(*args, limit):
         return True
     else:
         return False
+
+# return queryset of all mealitems found at meal at selected date
+def find_mealitems(user_id, product_id, date):
+    # firstly - find all meals from that date
+    meals = Meal.objects.filter(user_id=user_id, date=date)
+    meals_id = (x.meal_id for x in meals)
+    # create query to find all mealitems with above meal_id
+    query = Q()
+    for x in meals_id:
+        query = Q(meal_id=x) | query
+    # find all mealitems
+    mealitems = MealItem.objects.filter(query, product_id=product_id)
+    return mealitems
