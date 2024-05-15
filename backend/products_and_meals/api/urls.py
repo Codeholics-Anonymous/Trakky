e5895cdfdfdac9109ed09c31ee043ea3126d5563
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-05-15 15:33:54 +0200

    get meal path changed

--- backend/products_and_meals/api/urls.py
+++ backend/products_and_meals/api/urls.py
@@ -12,9 +12,7 @@ from products_and_meals.api.views import (
                 api_create_demand_view,
                 #MEAL
                 api_detail_meal_view,
-                api_update_meal_view,
                 api_delete_meal_view,
-                api_create_meal_view,
                 #MEAL ITEM
                 api_detail_meal_item_view,
                 api_update_meal_item_view,
@@ -38,10 +36,8 @@ urlpatterns = [
     path('demand/<str:date>/', api_detail_demand_view, name='demand_detail'),
     path('create_demand/', api_create_demand_view, name='demand_create'),
     #MEAL
-    path('meal/<int:meal_id>/', api_detail_meal_view, name='meal_detail'),
-    path('meal/<int:meal_id>/update/', api_update_meal_view, name='meal_update'),
+    path('meal/<str:date>/', api_detail_meal_view, name='meal_detail'),
     path('meal/<int:meal_id>/delete/', api_delete_meal_view, name='meal_delete'),
-    path('create_meal/', api_create_meal_view, name='meal_create'),
     #MEAL ITEM
     path('mealitem/<int:meal_item_id>/', api_detail_meal_item_view, name='meal_item_detail'),
     path('mealitem/<int:meal_item_id>/update/', api_update_meal_item_view, name='meal_item_update'),
