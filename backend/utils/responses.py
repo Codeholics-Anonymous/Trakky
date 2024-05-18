bf02448c7e3d375d5a9aa7476c57a67d03d69c56
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-05-18 18:53:15 +0200

    custom_response changed to short_response

--- backend/utils/responses.py
+++ backend/utils/responses.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework import status
 
-def custom_response(object, message, status_code=status.HTTP_200_OK):
+def short_response(object, message, status_code=status.HTTP_200_OK):
     return Response({object : message}, status=status_code)
 
 def not_found_response():
