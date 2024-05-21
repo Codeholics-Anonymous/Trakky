93fa8775ab4daa3b94278f7283e1da40aa0dfa3c
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-05-21 18:51:32 +0200

    convert integer values of work_type and user_goal fields into string values for user

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -10,4 +10,26 @@ class UserSerializer(serializers.ModelSerializer):
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserProfile 
-        fields = ['user_id', 'sex', 'weight', 'height', 'work_type', 'birth_date', 'daily_calory_demand', 'user_goal']
+        exclude = ['user', 'daily_calory_demand']
+
+    def get_user_goal_description(self, obj):
+        if obj.user_goal == -1:
+            return "Lose Weight"
+        elif obj.user_goal == 0:
+            return "Maintain Weight"
+        elif obj.user_goal == 1:
+            return "Gain Weight"
+        return "Unknown Goal"
+
+    def get_work_type_description(self, obj):
+        if obj.work_type == 0:
+            return "Mental Work"
+        elif obj.work_type == 1:
+            return "Physical Work"
+        return "Unknown Work"
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['work_type'] = self.get_work_type_description(instance)
+        representation['user_goal'] = self.get_user_goal_description(instance)
+        return representation
