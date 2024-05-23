b917d69e4156ba95936f36f6a3efe65ccf1bce28
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-05-23 11:13:11 +0200

    url to check if user is product manager

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from user.views import (api_update_userprofile_view, api_detail_userprofile_view)
+from user.views import (api_update_userprofile_view, api_detail_userprofile_view, api_is_product_manager_view)
 
 app_name = 'user'
 
 urlpatterns = [
     path('userprofile/', api_detail_userprofile_view, name='userprofile_detail'),
-    path('userprofile/update/', api_update_userprofile_view, name='userprofile_update')
+    path('userprofile/update/', api_update_userprofile_view, name='userprofile_update'),
+    path('is_product_manager/', api_is_product_manager_view)
 ]
