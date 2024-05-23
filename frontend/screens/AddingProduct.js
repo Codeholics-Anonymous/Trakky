e34adfc13888f93b61b0b15a65738c9de53f8571
j3drzejo <jirla30@gmail.com>
2024-05-23 11:12:54 +0200

    changed something

--- frontend/screens/AddingProduct.js
+++ frontend/screens/AddingProduct.js
@@ -1,9 +1,25 @@
-import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import axios from 'axios';
+import { Text, View, TextInput, TouchableOpacity, Button } from 'react-native';
 
 export function AddingProduct({ navigation }) {
+  const token = "7b903ccfd8d8596d45d4b4a3edd43f4d4c58865e";
+
+  const handlePress = () => {
+      axios.get('https://trakky.onrender.com/api/basic_demand/', {
+        headers: {
+          'Authorization' : 'Token ' + token
+        }
+      }).then((response) =>{
+        console.log(response.data);
+      }, (error) => {
+        console.log(error)
+        Alert.alert("Error, try again")
+      })
+  }
+  
   return (
     <View>
-      <Text>add</Text>
+      <Button onPress={handlePress} title="aaa" />
     </View>
   )
 }
