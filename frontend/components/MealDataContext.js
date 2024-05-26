658809bd0edc08168f3aa3fc09bbf7ef9b4e93c9
iamk-cper <kacper.mycka02@gmail.com>
2024-05-26 13:38:49 +0200

    MealItem search bar, POST MealItems into Meal, date selection and arrow visibility lock at current date, circle bar lock on 100 of progress

--- frontend/components/MealDataContext.js
+++ frontend/components/MealDataContext.js
@@ -13,6 +13,8 @@ export const MealDataProvider = ({ children }) => {
     dinner: {}
   });
   const [date, setDate] = useState(new Date());
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchError, setSearchError] = useState(''); // State to store search error
 
   const fetchData = async (selectedDate) => {
     try {
@@ -22,7 +24,7 @@ export const MealDataProvider = ({ children }) => {
         return;
       }
       const dateString = `${selectedDate.getFullYear()}-${selectedDate.getMonth() + 1}-${selectedDate.getDate()}`;
-      const url = `https://trakky.onrender.com/api/meal/${dateString}`;
+      const url = `https://trakky.onrender.com/api/meal/${dateString}/`;
 
       const response = await axios.get(url, {
         headers: {
@@ -40,12 +42,67 @@ export const MealDataProvider = ({ children }) => {
     }
   };
 
+  const searchProducts = async (query) => {
+    setSearchError(''); // Clear previous errors
+    try {
+      const { token } = await getUserData();
+      if (!token) {
+        console.error("No token found. Please login again.");
+        return;
+      }
+      const url = `https://trakky.onrender.com/api/product/${query}/`;
+      const response = await axios.get(url, {
+        headers: {
+          'Authorization': `Token ${token}`
+        }
+      });
+      setSearchResults(response.data);
+      if (response.data.length === 0) {
+        setSearchError('Product not found. Add product in settings menu.');
+      }
+    } catch (error) {
+      if (error.response && error.response.status === 404) {
+        setSearchError('Product not found. Add product in settings menu.');
+      } else {
+        setSearchError('Error searching products');
+      }
+      setSearchResults([]);
+    }
+  };
+
+  const createMealItem = async (mealType, date, productId, gramAmount) => {
+    try {
+      const { token } = await getUserData();
+      if (!token) {
+        console.error("No token found. Please login again.");
+        return;
+      }
+      const url = `https://trakky.onrender.com/api/create_mealitem/${mealType}/${date}/`;
+      const response = await axios.post(url, {
+        "product_id": productId,
+        "gram_amount": gramAmount
+      }, {
+        headers: {
+          'Authorization': `Token ${token}`
+        }
+      });
+
+      // Refresh meal data after creating a meal item
+      fetchData(new Date(date));
+    } catch (error) {
+      console.error("Error creating meal item:", error);
+      if (error.response) {
+        console.error(`Response Error: ${error.response.status} ${JSON.stringify(error.response.data)}`);
+      }
+    }
+  };
+
   useEffect(() => {
     fetchData(date); // Trigger a fetch when the date changes
   }, [date]);
 
   return (
-    <MealDataContext.Provider value={{ mealData, setMealData, date, setDate }}>
+    <MealDataContext.Provider value={{ mealData, setMealData, date, setDate, searchResults, searchProducts, searchError, createMealItem }}>
       {children}
     </MealDataContext.Provider>
   );
