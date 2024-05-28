d627bad6af0fd89fb6ec82275ffccd40250d86e5
Blizni13 <szymo@DESKTOP-V0MIKMF>
2024-05-28 21:57:09 +0200

    get account creation date view and url

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from user.views import (api_update_userprofile_view, api_detail_userprofile_view, api_is_product_manager_view)
+from user.views import (api_update_userprofile_view, api_detail_userprofile_view, api_is_product_manager_view, api_get_account_creation_date_view)
 
 app_name = 'user'
 
 urlpatterns = [
     path('userprofile/', api_detail_userprofile_view, name='userprofile_detail'),
     path('userprofile/update/', api_update_userprofile_view, name='userprofile_update'),
-    path('is_product_manager/', api_is_product_manager_view)
+    path('is_product_manager/', api_is_product_manager_view),
+    path('account_creation_date/', api_get_account_creation_date_view),
 ]
